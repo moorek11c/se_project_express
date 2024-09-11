05bfa4c85ad8600b976496d25e080f98715cf49b
Ryan Moore <moorek11c@gmail.com>
2024-09-11 11:33:23 -0700

    added .env import

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 const app = express();
+
 const indexRoutes = require("./routes/index");
 
 const {
@@ -12,6 +14,8 @@ const {
   ERROR_MESSAGES,
   CustomError,
 } = require("./utils/errors");
+
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db", {
@@ -28,11 +32,15 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
+app.use(requestLogger);
+
 app.use("/", indexRoutes);
 
 app.use((req, res, next) => {
   next(new CustomError(ERROR_MESSAGES.INVALID_ROUTER, ERROR_CODES.NOT_FOUND));
 });
+
+app.use(errorLogger);
 
 app.use(errorHandler);
 
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
-const JWT_SECRET = "this is a secret key";
+const crypto = require("crypto");
+
+const { JWT_SECRET = "this is a secret key" } = process.env;
 
 module.exports = { JWT_SECRET };
